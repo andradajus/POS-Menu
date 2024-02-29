251eefa9473b0ca3dd85196db48977d1eec1af25
andradajus <138541478+andradajus@users.noreply.github.com>
2024-02-29 12:23:30 +0800

    Added masking of cells on product table, with dummy data

--- react/src/client/menu/MenuTable.jsx
+++ react/src/client/menu/MenuTable.jsx
@@ -1,81 +1,144 @@
-import InputModal from "../../components/InputModal"
-import { AddFoodInputs } from "../../constants/inputs"
+import InputModal from "../../components/InputModal";
+import { AddFoodInputs } from "../../constants/inputs";
+import { useState } from "react";
 const MenuTable = () => {
-    const inputs = AddFoodInputs
-    const handleAddProduct = () => {
-        const modal = document.getElementById('addProductModal');
-        if (modal) {
-            modal.showModal();
-        }
+  const inputs = AddFoodInputs;
+  const [editableCell, setEditableCell] = useState(null);
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "Product Name",
+      description: "Description",
+      category: "Category",
+      price: "Price",
+      stock: "Stock",
+    },
+  ]);
+  const handleAddProduct = () => {
+    const modal = document.getElementById("addProductModal");
+    if (modal) {
+      modal.showModal();
     }
+  };
 
-    const handleFormSubmit = (formData) => {
-        console.log('Form submitted with data:', formData);
-    };
+  const handleEditClick = (index) => {
+    setEditableCell(index);
+  };
 
-    console.log("inputs", inputs)
+  const handleInputChange = (event, index, field) => {
+    const updatedProducts = [...products];
+    updatedProducts[index][field] = event.target.value;
+    setProducts(updatedProducts);
+  };
 
-    return (
-        <>
-            <div className="flex justify-between">
-                <button
-                className="btn btn-primary m-3"
-                onClick={handleAddProduct}
-                >
-                Add Product
-                </button>
-                <div className="flex align-middl text-2xl mt-4 font-bold">Product Table</div>
-                <div></div>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Category</th>
-                            <th>Price</th>
-                            <th>Stock</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>
-                            <label>
-                                1
-                            </label>
-                            </th>
-                            <td>
-                            <div className="flex items-center gap-3">
-                                <div className="avatar">
-                                    <div className="mask mask-squircle w-12 h-12">
-                                        <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div className="font-bold">Product Name</div>
-                                    <div className="text-sm opacity-50">Description</div>
-                                </div>
-                            </div>
-                            </td>
-                            <td>Category</td>
-                            <td>Price</td>
-                            <th>Stock</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+  const handleInputBlur = () => {
+    setEditableCell(null);
+  };
 
-            <dialog id="addProductModal" className="modal">
-                <div className="modal-box">
-                    <form method="dialog">
-                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => document.getElementById('my_modal_3').close()}>✕</button>
-                    </form>
-                    <InputModal onSubmit={handleFormSubmit} inputs={inputs} />
-                </div>
-            </dialog>
-        </>
-    )
-}
+  const handleFormSubmit = (formData) => {
+    console.log("Form submitted with data:", formData);
+  };
 
-export default MenuTable
+  console.log("inputs", inputs);
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <button className="btn btn-primary m-3" onClick={handleAddProduct}>
+          Add Product
+        </button>
+        <div className="flex align-middl text-2xl mt-4 font-bold">
+          Product Table
+        </div>
+        <div></div>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => (
+              <tr key={index}>
+                <th>
+                  <label>{product.id}</label>
+                </th>
+                <td>
+                  {editableCell === index ? (
+                    <>
+                      <div className="avatar"></div>
+                      <input
+                        type="text"
+                        value={product.name}
+                        onChange={(e) => handleInputChange(e, index, "name")}
+                        onBlur={handleInputBlur}
+                      />
+                    </>
+                  ) : (
+                    <div
+                      className="font-bold flex "
+                      onClick={() => handleEditClick(index)}
+                    >
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-2@56w.png"
+                          alt="IMG"
+                        />
+                      </div>
+                      {product.name}
+                    </div>
+                  )}
+                </td>
+                <td>
+                  {editableCell === index ? (
+                    <input
+                      type="text"
+                      value={product.description}
+                      onChange={(e) =>
+                        handleInputChange(e, index, "description")
+                      }
+                      onBlur={handleInputBlur}
+                    />
+                  ) : (
+                    <div
+                      className="font-bold"
+                      onClick={() => handleEditClick(index)}
+                    >
+                      {product.description}
+                    </div>
+                  )}
+                </td>
+                <td>{product.category}</td>
+                <td>{product.price}</td>
+                <td>{product.stock}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <dialog id="addProductModal" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
+              ✕
+            </button>
+          </form>
+          <InputModal onSubmit={handleFormSubmit} inputs={inputs} />
+        </div>
+      </dialog>
+    </>
+  );
+};
+
+export default MenuTable;
